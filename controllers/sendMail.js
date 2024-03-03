e16025c32a55eda34854c7eea836ab4d40dd5973
Raj2902 <rajagarwalgood@gmail.com>
2024-03-03 17:53:56 +0530

    Send mail fixed and made template

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sendMailFunction = (req, res) => {
+const sendMailFunction = async (req, res) => {
   // Credentials for a trusted email service provider (replace with your own)
   const transporter = nodemailer.createTransport({
     service: "gmail", // Use a reliable service like Gmail or SendGrid
@@ -12,18 +12,53 @@ const sendMailFunction = (req, res) => {
 
   const mailOptions = {
     from: '"Raj Agarwal" <rajagarwalgood@gmail.com>',
-    to: "rajagarwalawesom29@gmail.com",
-    subject: "Test Email from Node.js",
-    text: "This is a plain text email sent using Nodemailer.",
+    to: req.body.email,
+    subject: "A question has been asked from your portfolio",
+    html: `<div style="font-family: sans-serif">
+        <div style="text-align: center">
+          <strong style="font-size: xx-large"
+            >You got a question to answer</strong
+          >
+        </div>
+        <div style="display: flex; justify-content: center">
+          <div
+            style="
+              border: 1px solid grey;
+              padding: 10px;
+              text-align: center;
+              display: inline-block;
+              border-radius: 3%;
+              margin-top: 2%;
+              line-height: 2;
+            "
+          >
+            <b style="font-size: x-large">Contact Form</b>
+            <p>
+              Name : ${req.body.name}<br />Email : ${req.body.email}<br />Subject :
+              ${req.body.subject}<br />Message : ${req.body.message}
+            </p>
+            <p>Thanks,<br />Raj Agarwal</p>
+          </div>
+        </div>
+        <p style="color: grey; text-align: center; margin-top: 4%">
+          You're receiving this email because someone has send a message from your
+          portfolio contact form.
+        </p>
+      </div>`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log("Email sent:", info.response);
-      res.status(200).json({ message: "Mail sent", body: info.response });
-    }
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+        res.status(400).json({ error: error });
+      } else {
+        console.log("Email sent:", info.response);
+        resolve(info.response);
+        res.status(200).json({ message: "Mail sent", body: info.response });
+      }
+    });
   });
 };
 
